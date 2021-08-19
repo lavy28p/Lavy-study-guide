072af60c7556eba920b6c3bf29dbf623e370dbbc
lavy2p <lavy28p@gmail.com>
2021-08-19 14:53:54 -0500

    added models and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Answer.destroy_all
+Question.destroy_all
+User.destroy_all
+
+# Create User
+
+user1 = User.create(username: 'Lavy', email: 'lavy@test.com', password_digest: 'Lavy123', is_admin: true)
+
+puts "#{User.count} users created"
+
+# Create Question
+
+question1 = Question.create(post: 'What are ruby datatypes?', user: user1)
+
+puts "#{Question.count} questions created"
+
+
+#Create Answer
+
+answer1 = Answer.create(comment: 'Numbers, Strings, Symbols, Hashes, Arrays, Booleans', user: user1, question: question1)
+
+puts "#{Answer.count} answers created"
