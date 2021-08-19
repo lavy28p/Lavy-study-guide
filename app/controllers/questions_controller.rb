e504899babdcbaa2e54f58bb99b8f11887237ab2
lavy2p <lavy28p@gmail.com>
2021-08-19 17:32:42 -0500

    done backend

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+  # before_action :set_list, only: [:show, :update, :destroy, :add_list]
+  # before_action :authorize_request, only: [ :show, :create, :update, :index, :destroy]
+  
+  # GET /questions
+  def index
+    @questions = Question.all
+    render json: @questions, include: :user
+  end
+
+  # GET /questions/id
+  def show
+    render json: @question, include: {answers:{include: :user}}
+  end
+
+# POST /questions
+  def create
+    @question = Question.new(question_params)
+    @question.user = @current_user
+    if @question.save
+      render json: @question, status: :created
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
+  end
+
+# PATCH/PUT /question/id
+  def update
+    if @question.update(question_params)
+      render json: @question, status: :ok
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
+  end
+
+# DELETE /question/id
+def destroy
+  @question.destroy
+end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:post, :user_id)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,7 @@ puts "#{User.count} users created"
 
 question1 = Question.create(post: 'What are ruby datatypes?', user: user1)
 question2 = Question.create(post: 'What is the difference between calling super and calling super()?', user: user1)
+question3 = Question.create(post: 'How to seed data in rails psql db?', user: user1)
 
 puts "#{Question.count} questions created"
 
@@ -27,6 +28,7 @@ puts "#{Question.count} questions created"
 #Create Answer
 
 answer1 = Answer.create(comment: 'Numbers, Strings, Symbols, Hashes, Arrays, Booleans', user: user1, question: question1)
-answer1 = Answer.create(comment: 'super invokes the parent method with the same arguments that were passed to the child method.A super() invokes the parent method without any arguments', user: user1, question: question2)
+answer2 = Answer.create(comment: 'super invokes the parent method with the same arguments that were passed to the child method.A super() invokes the parent method without any arguments', user: user1, question: question2)
+answer3 = Answer.create(comment: 'rails db:reset', user: user1, question: question3)
 
 puts "#{Answer.count} answers created"
